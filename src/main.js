84babd343ef2ed7a227cbdf8a7e71a699a4bc07b
bernardoenock <bernardoenockgm@gmail.com>
2024-12-18 02:06:40 -0300

    feat: Add ES6 and function compileAMPscript

--- src/main.js
+++ src/main.js
@@ -1,31 +1,25 @@
-const path = require('path');
-const fs = require('fs');
-const AMPscriptParser = require(path.resolve(__dirname, 'parser.js'));
-const AMPscriptCompiler = require(path.resolve(__dirname, 'compiler.js'));
+import { AMPscriptParser } from './parser.js';
+import { AMPscriptCompiler } from './compiler.js';
 
-const inputScript = fs.readFileSync('./examples/input.ampscript.txt', 'utf-8');
+export default function compileAMPscript() {
+  const inputScript = document.getElementById('ampscriptInput').value.toString();
 
-// Separar o script AMPscript do HTML
-const match = inputScript.match(/%%\[\s*([\s\S]*?)\s*\]%%\s*([\s\S]*)/);
-console.log('Conteúdo do arquivo:', inputScript);
-console.log('MATCH:', match);
-if (!match) {
-  throw new Error('O arquivo de entrada não está no formato esperado.');
+  const match = inputScript.match(/%%\[\s*([\s\S]*?)\s*\]%%\s*([\s\S]*)/);
+
+  if (!match) {
+    alert('O texto de entrada não está no formato esperado.');
+    return;
+  }
+
+  const ampscript = match[1];
+  const template = match[2];
+
+  const parser = new AMPscriptParser(ampscript);
+  parser.parse();
+
+  const compiler = new AMPscriptCompiler(template, parser.getVariables());
+  const compiledHTML = compiler.compile();
+
+  const outputContainer = document.getElementById('outputHTML');
+  outputContainer.innerHTML = compiledHTML;
 }
-
-const ampscript = match[1];
-const template = match[2];
-console.log('AMPscript:', ampscript);
-console.log('Template:', template); // Adicionado para depuração
-
-// Processa a lógica AMPscript
-const parser = new AMPscriptParser(ampscript);
-parser.parse();
-
-// Compila o HTML
-const compiler = new AMPscriptCompiler(template, parser.getVariables());
-const compiledHTML = compiler.compile();
-
-// Salva a saída
-fs.writeFileSync('./examples/output.html', compiledHTML);
-console.log('HTML gerado com sucesso!');
